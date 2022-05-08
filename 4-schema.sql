e4cad46925cb4d96e8ebaf3ec0bb7546fd85f5d8
AmiraZidni <zidni.imani@gmail.com>
2022-05-08 13:41:25 +0700

    typo exist to exists

--- 4-schema.sql
+++ 4-schema.sql
@@ -1,7 +1,7 @@
-CREATE DATABASE IF NOT EXIST hello_world;
+CREATE DATABASE IF NOT EXISTS hello_world;
 use hello_world;
 
-CREATE TABLE IF NOT EXIST users(
+CREATE TABLE IF NOT EXISTS users(
     id int NOT NULL AUTO_INCREMENT,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL,
